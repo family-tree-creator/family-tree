e9b524ad2d190013e1594e78babcdeea3e48c08c
dpositeri <dpositeri@gmail.com>
2015-02-25 21:27:49 -0800

    singleTree. addChild (last case doesn’t work yet).

--- famtree.hs
+++ famtree.hs
@@ -3,23 +3,40 @@
 --
 
 --tree in three forms: empty, single individual, married w/ children
-data FamTree a = Empty | Node [FamTree a]
+data FamTree a = Empty | Node a [FamTree a]
   deriving (Show, Read, Eq)
 
+--tree for single individual
+singleTree :: a -> FamTree a
+singleTree a = Node a []
 
+--data FamTree a = Empty | Node {
+--		root :: a,
+--		children :: FamTree a 
+--		} deriving (Show, Read, Eq) 
+
+--add child to parent
+addChild :: (Eq a) => a -> FamTree a -> FamTree a
+addChild c Empty = singleTree c
+addChild c (Node p list) 
+	| c == p = singleTree p
+	| list == [] = Node p (Node c[]:[]) 
+	| c == (list:_) = Node p list
+
+--should Individual be type instead? 
+--Individual :: [(String,String,[Int],[Int],Int)]
 data Individual = Individual { firstName :: String
 						     , lastName :: String
 						     , birth :: [Int]
 						     , death :: [Int]
 						     , age :: Int
-						   } deriving (Show)
+						   } deriving (Show, Read, Eq)
 						   
 printIndividual :: Individual -> String
 printIndividual (Individual {firstName = f,lastName = l, birth = b, death = d, age = a}) = f ++
      " , " ++ l ++ ". Birth date: " ++ show b ++ " Death date: " ++ show d ++" Age: " ++ show a
  
---error: unexpected kind variable 'a'.  
---data Node :: FamTree a -> FamTree a -> FamTree a 
+
  
 --prints curr or root of tree
 --toList :: famTree a -> Node 
