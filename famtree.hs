9446cde567d1b692825896238b9fcd724b129583
dpositeri <dpositeri@gmail.com>
2015-03-01 23:17:53 -0800

    addParent + test variables

--- famtree.hs
+++ famtree.hs
@@ -3,7 +3,7 @@
 --
 
 --tree in three forms: empty, single individual, married w/ children
-data FamTree a = Empty | Node a [FamTree a]
+data FamTree a = Empty | Node a [FamTree a] 
   deriving (Show, Read, Eq)
 
 --tree for single individual
@@ -15,7 +15,10 @@ singleTree a = Node a []
 --		children :: FamTree a 
 --		} deriving (Show, Read, Eq) 
 
---add child to parent
+-- add child to parent: (delete?)
+-- child added can just be String
+-- >addChild "child" (Node "parent" []) 
+-- | Node "parent" [Node "child []]
 addChild :: (Eq a) => a -> FamTree a -> FamTree a
 addChild c Empty = singleTree c
 addChild c (Node p list) 
@@ -24,7 +27,8 @@ addChild c (Node p list)
 	| c `famElem` list = Node p list
 	| otherwise = Node p (list ++ [Node c[]])
 
---add child version 2
+--add child version 2: child added is already a Node
+-- >addChild' (Node "child" []) (Node "parent" [])
 addChild' :: (Eq a) => FamTree a -> FamTree a -> FamTree a
 addChild' (Node c cList) Empty = (Node c cList)
 addChild' (Node c cList) (Node p list) 
@@ -32,6 +36,22 @@ addChild' (Node c cList) (Node p list)
 	| list == [] = Node p (Node c cList:[])
 	| c `famElem` list = Node p list   				--erases cList
 	| otherwise = Node p (list ++ [Node c cList])
+
+--add parent to child. 
+-- parent added just String (delete?)
+addParent :: (Eq a) => a -> FamTree a -> FamTree a
+addParent p Empty = singleTree p
+addParent p (Node c cList) 
+	| p == c = (Node c cList)
+	| otherwise = Node p (Node c cList:[])
+
+--add parent version 2: Node parent added to Node child
+addParent' :: (Eq a) => FamTree a -> FamTree a -> FamTree a
+addParent' (Node p list) Empty = (Node p list)
+addParent' (Node p list) (Node c cList)
+	| p == c = (Node c cList)
+	| otherwise = addChild' (Node c cList) (Node p list)
+
 
 --checks if element is part of a family tree
 famElem :: (Eq a) => a -> [FamTree a] -> Bool
@@ -41,9 +61,7 @@ famElem a (Node x(xChild):xs)
 	| xChild /= [] = a `famElem` xChild
 	| otherwise = a `famElem` xs 
 
-
---should Individual be type instead? 
---Individual :: [(String,String,[Int],[Int],Int)]
+--datatype to hold information about individual
 data Individual = Individual { firstName :: String
 						     , lastName :: String
 						     , birth :: [Int]
@@ -53,15 +71,14 @@ data Individual = Individual { firstName :: String
 						   
 printIndividual :: Individual -> String
 printIndividual (Individual {firstName = f,lastName = l, birth = b, death = d, age = a}) = f ++
-     " , " ++ l ++ ". Birth date: " ++ show b ++ " Death date: " ++ show d ++" Age: " ++ show a
+     " , " ++ l ++ ". Birth date: " ++ show b ++ " Death date: " ++ show d ++" Age: " ++ show a 
  
+--prints curr or root of tree. Not useful yet
+printRoot :: FamTree a -> FamTree a 
+printRoot Empty = Empty
+printRoot (Node x list) = Node x []
 
- 
---prints curr or root of tree
---toList :: famTree a -> Node 
---toList (Empty) = Empty
---toList (Node) = Node
---toList (Node (xs)) = Node 
-
---sample = Node "Will" [Node "Willow" [], Node "Jaden" []]
-
+--test variables
+a = Node "Ann" []
+b = Individual "Ann" "Whal" [01,24,1980] [04,14,2010] 30
+sample = Node "Will" [Node "Willow" [], Node "Jaden" []]
